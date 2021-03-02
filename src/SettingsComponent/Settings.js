187205bc9d709ed55cf064ffd03375ae55ebfa4a
ifuncuran <ifuncuran@gmail.com>
2021-03-02 09:10:15 +0300

    add input in settings

--- src/SettingsComponent/Settings.js
+++ src/SettingsComponent/Settings.js
@@ -3,19 +3,44 @@ import React, { useState } from 'react';
 const Settings = (props) => {
   const [showSettings, ChangeShowSettings] = useState(false);
 
+  const [login, ChangeLoginHandler] = useState('ifuncuran');
+  const [repo, ChangeRepoHandler] = useState('repo');
+  const [blacklist, ChangeBlacklistHandler] = useState('blacklist');
+
   let settings = null;
 
   if (showSettings) {
     settings = (
       <div className="settings__container">
         <div className="settings__row">
-          <div className="login">login</div>
+          <div className="login">
+            <input
+              type="text"
+              onChange={(event) => ChangeLoginHandler(event.target.value)}
+              value={login}
+              className="input"
+            />
+          </div>
         </div>
         <div className="settings__row">
-          <div className="repo">repo</div>
+          <div className="repo">
+            <input
+              type="text"
+              onChange={(event) => ChangeRepoHandler(event.target.value)}
+              value={repo}
+              className="input"
+            />
+          </div>
         </div>
         <div className="settings__row">
-          <div className="blackList">blackList</div>
+          <div className="blackList">
+            <input
+              type="text"
+              onChange={(event) => ChangeBlacklistHandler(event.target.value)}
+              value={blacklist}
+              className="input"
+            />
+          </div>
         </div>
         <div className="settings__row">
           <div className="buttons">
